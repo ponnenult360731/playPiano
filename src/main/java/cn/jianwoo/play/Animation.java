3945712d7212d98a97f29b5c5bfe23cd8009308a
chendong <chendong@focoon.com.cn>
2022-11-29 09:40:49 +0800

    格式化

--- src/main/java/cn/jianwoo/play/Animation.java
+++ src/main/java/cn/jianwoo/play/Animation.java
@@ -5,11 +5,15 @@ package cn.jianwoo.play;
  * @Description
  * @date 2022-11-15 00:58
  */
-public class Animation extends Thread{
+public class Animation extends Thread {
 
-    /** 音符 */
+    /**
+     * 音符
+     */
     private String[] notes;
-    /** 间隔时间（单位：毫秒） */
+    /**
+     * 间隔时间（单位：毫秒）
+     */
     private int times;
 
     public Animation(int times) {
@@ -50,20 +54,18 @@ public class Animation extends Thread{
             sleep(1000);
             int no = 1;
 //            System.out.print(no+": ");
-            for (int i = 0; i < this.notes.length; i++)
-            {
-                if (notes[i].length()<1){
+            for (int i = 0; i < this.notes.length; i++) {
+                if (notes[i].length() < 1) {
                     continue;
                 }
-                String n = this.notes[i].replace("+","").replace("-","");
-                if (n.equals("\n")||n.equals("\r")){
+                String n = this.notes[i].replace("+", "").replace("-", "");
+                if (n.equals("\n") || n.equals("\r")) {
                     System.out.print("\n");
                     no++;
 //                    System.out.print(no+": ");
                     continue;
                 }
-                switch (n)
-                {
+                switch (n) {
                     case "0":
                         System.out.print("_");
                         break;
--- src/main/java/cn/jianwoo/play/Audio.java
+++ src/main/java/cn/jianwoo/play/Audio.java
@@ -8,35 +8,26 @@ import cn.hutool.core.io.resource.ResourceUtil;
 import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
 
-public class Audio
-{
+public class Audio {
     private static InputStream is;
     private Player player;
     ExecutorService service = Executors.newCachedThreadPool();
 
-    public Audio(String path)
-    {
+    public Audio(String path) {
         is = ResourceUtil.getStream(path);
-        try
-        {
+        try {
             player = new Player(is);
-        }
-        catch (JavaLayerException e)
-        {
+        } catch (JavaLayerException e) {
             e.printStackTrace();
         }
     }
 
 
-    public void start()
-    {
+    public void start() {
         service.submit(() -> {
-            try
-            {
+            try {
                 player.play();
-            }
-            catch (JavaLayerException e)
-            {
+            } catch (JavaLayerException e) {
 
             }
         });
